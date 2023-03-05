2d8cc4da09a87947dfe42433e1bedb833e22804b
94726 <maik.s.kowol@gmail.com>
2023-03-05 14:19:52 +0100

    fix(laufsteg-vue): linting issues

--- packages/laufsteg-vue/.eslintrc.cjs
+++ packages/laufsteg-vue/.eslintrc.cjs
@@ -1,13 +1,19 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
+const rootConfig = require('../../.eslintrc.cjs');
 
 module.exports = {
+  root: true,
+  ...rootConfig,
   parser: 'vue-eslint-parser',
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
   ],
+  plugins: ['simple-import-sort', 'import'],
   rules: {
+    ...rootConfig.rules,
     'vue/multi-word-component-names': 0,
   },
 };
